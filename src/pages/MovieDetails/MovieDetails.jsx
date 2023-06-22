a5aabe1defc20ec851b2ad6bf4bb257b11a32df3
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-06-22 22:00:54 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDetails } from 'utils/fetchs';
 
 export const MovieDetails = () => {
     const { movieId } = useParams();
-    console.log(movieId);
+    
+
+    useEffect(() => {
+        getDetails(movieId)
+    
+    })
 };
